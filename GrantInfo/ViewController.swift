737c80261b0b3224024ed68df2f90fdf32f78297
TakJewon <taknineball@castis.com>
2024-09-30 15:20:41 +0900

    Initial Commit

---
+++ GrantInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GrantInfo
+//
+//  Created by 탁제원 on 9/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GrantInfoTests/GrantInfoTests.swift
@@ -0,0 +1,17 @@
+//
+//  GrantInfoTests.swift
+//  GrantInfoTests
+//
+//  Created by 탁제원 on 9/30/24.
+//
+
+import Testing
+@testable import GrantInfo
+
+struct GrantInfoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
